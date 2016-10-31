26944e22c7d70d2179f9fd9cd6d3a31c20f277e2
Simon Wendel <mail@simonwendel.se>
2016-10-31 18:24:29 +0100

    feat: Add empty projects

---
+++ src/SimonWendel.GuardStatements.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Guard Statements - Guarding invariants in .NET
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimonWendel.GuardStatements.Tests")]
+[assembly: AssemblyDescription("Unit tests for the SimonWendel.GuardStatements assembly.")]
+[assembly: AssemblyProduct("SimonWendel.GuardStatements.Tests")]
+[assembly: AssemblyCopyright("Copyright © Simon Wendel 2016")]
+[assembly: ComVisible(false)]
+[assembly: Guid("9bcf319c-1df8-40b2-8347-7c1d5a09975c")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/SimonWendel.GuardStatements/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Guard Statements - Guarding invariants in .NET
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimonWendel.GuardStatements")]
+[assembly: AssemblyDescription("Guard statements enforcing invariants.")]
+[assembly: AssemblyProduct("SimonWendel.GuardStatements")]
+[assembly: AssemblyCopyright("Copyright © Simon Wendel 2016")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e8f3a40f-89d9-4acb-b091-bec54e73a869")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
