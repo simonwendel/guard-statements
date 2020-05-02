2a82dba03aaa1cf011f3bb384417015a8ed3267b
Simon Wendel <mail@simonwendel.se>
2020-05-02 10:14:07 +0200

    chore: rename namespaces, dropping author name
    This is probably a breaking change.

--- src/GuardStatements.Tests/GuardTests.cs
+++ src/GuardStatements.Tests/GuardTests.cs
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace SimonWendel.GuardStatements.Tests
+namespace GuardStatements.Tests
 {
     using System;
     using FluentAssertions;
